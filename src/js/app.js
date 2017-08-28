d72de7985f746e9e02de6901075b7772d67bae1c
benlloydjones <benlloydjones@gmail.com>
2017-08-28 07:26:31 +0100

    Monday morning

--- src/js/app.js
+++ src/js/app.js
@@ -42,6 +42,106 @@ let nodeFrom = null;
 
 //functions go here
 
+//if all nodes of a certain class are in a mill this will return true
+function canRemoveMill(colour) {
+  const nodesToCheck = [];
+  const nodes = [$node1, $node2, $node3, $node4, $node5, $node6, $node7, $node8, $node9, $node10, $node11, $node12, $node13, $node14, $node15, $node16, $node17, $node18, $node19, $node20, $node21, $node22, $node23, $node24];
+  nodes.forEach((node) => {
+    if(node.hasClass(colour)) {
+      nodesToCheck.push(node);
+    }
+  });
+  return nodesToCheck.every((node) => isInMill(node));
+}
+
+//this checks an insterted node and returns true if it is in a mill
+function isInMill(nodeToTest) {
+  //we control the horizontal
+  if([$node1[0], $node2[0], $node3[0]].includes(nodeToTest[0])) {
+    if([$node1, $node2, $node3].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node9[0], $node10[0], $node11[0]].includes(nodeToTest[0])) {
+    if([$node9, $node10, $node11].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node17[0], $node18[0], $node19[0]].includes(nodeToTest[0])) {
+    if([$node17, $node18, $node19].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node4[0], $node12[0], $node20[0]].includes(nodeToTest[0])) {
+    if([$node4, $node12, $node20].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node21[0], $node13[0], $node5[0]].includes(nodeToTest[0])) {
+    if([$node21, $node13, $node5].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node22[0], $node23[0], $node24[0]].includes(nodeToTest[0])) {
+    if([$node22, $node23, $node24].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node14[0], $node15[0], $node16[0]].includes(nodeToTest[0])) {
+    if([$node14, $node15, $node16].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node6[0], $node7[0], $node8[0]].includes(nodeToTest[0])) {
+    if([$node6, $node7, $node8].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  //we control the vertical
+  if([$node1[0], $node4[0], $node6[0]].includes(nodeToTest[0])) {
+    if([$node1, $node4, $node6].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node9[0], $node12[0], $node14[0]].includes(nodeToTest[0])) {
+    if([$node9, $node12, $node14].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node17[0], $node20[0], $node22[0]].includes(nodeToTest[0])) {
+    if([$node17, $node20, $node22].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node2[0], $node10[0], $node18[0]].includes(nodeToTest[0])) {
+    if([$node2, $node10, $node18].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node23[0], $node15[0], $node7[0]].includes(nodeToTest[0])) {
+    if([$node23, $node15, $node7].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node19[0], $node21[0], $node24[0]].includes(nodeToTest[0])) {
+    if([$node19, $node21, $node24].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node11[0], $node13[0], $node16[0]].includes(nodeToTest[0])) {
+    console.log('clicked');
+    if([$node11, $node13, $node16].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  if([$node3[0], $node5[0], $node8[0]].includes(nodeToTest[0])) {
+    if([$node3, $node5, $node8].every((node) => node.attr('class') === nodeToTest.attr('class'))) {
+      return true;
+    }
+  }
+  return false;
+}
+
 //this function checks if the node you've clicked on is in a mill and is used for both removing and adding counters
 function checkMill(e) {
   //we control the horizontal
@@ -147,20 +247,36 @@ function applyCounter(e) {
 
 //this function will remove a counter if that counter is not in a mill and increase the score of the player removing a counter
 function removeCounter(e) {
-  if(!checkMill(e) && ($(e.target).hasClass('green') || $(e.target).hasClass('purple'))) {
-    if($(e.target).hasClass('purple')) {
+  if(turnCounter % 2 !== 0 && canRemoveMill('purple')) {
+    if(!$(e.target).hasClass('purple')) {
+      console.log('You need to choose a purple node');
+    } else {
+      $(e.target).removeClass('purple');
+      greenPlayer++;
+      millCreated = false;
+    }
+  } else if (turnCounter % 2 === 0 && canRemoveMill('green')) {
+    if(!$(e.target).hasClass('green')) {
+      console.log('You need to choose a green node');
+    } else {
+      $(e.target).removeClass('green');
+      purplePlayer++;
+      millCreated = false;
+    }
+  } else if(!checkMill(e) && ($(e.target).hasClass('green') || $(e.target).hasClass('purple'))) {
+    if($(e.target).hasClass('purple') && turnCounter % 2 !== 0) {
       $(e.target).removeClass('purple');
       greenPlayer ++;
       millCreated = false;
-      console.log(`Green: ${greenPlayer}, Purple: ${purplePlayer}`);
-    } else if($(e.target).hasClass('green')) {
+    } else if($(e.target).hasClass('green') && turnCounter % 2 === 0) {
       $(e.target).removeClass('green');
       purplePlayer ++;
       millCreated = false;
-      console.log(`Green: ${greenPlayer}, Purple: ${purplePlayer}`);
     }
   } else if(checkMill(e)){
     console.log('this is in a mill');
+  } else if(($(e.target).hasClass('green') && turnCounter % 2 !== 0) || ($(e.target).hasClass('purple') && turnCounter % 2 === 0)) {
+    console.log('You need to choose a token from the other player');
   } else {
     console.log('That node does not contain a counter!');
   }
@@ -184,17 +300,6 @@ function pickUpCounter(e) {
     console.log('this node has the wrong colour counter');
   }
 }
-
-// if($(e.target).hasClass('purple')) {
-//   $(e.target).removeClass('purple');
-//   counterInHand = 'purple';
-//   movingCounter = true;
-// } else if($(e.target).hasClass('green')) {
-//   $(e.target).removeClass('green');
-//   counterInHand = 'green';
-//   movingCounter = true;
-// }
-// trackCounter(e);
 
 //function to place counter only if in a valid square when being moved
 function placeCounter(e) {
@@ -261,19 +366,21 @@ function fairMove(e) {
   }
 }
 
+//Creates next turn
 function newTurn() {
   turnCounter++;
   if(turnCounter % 2 === 0) {
     $playerDisplay.text('Purple');
     $body.removeClass('green');
     $body.addClass('purple');
-    $turnDisplay.text(turnCounter);
   } else {
     $playerDisplay.text('Green');
     $body.removeClass('purple');
     $body.addClass('green');
-    $turnDisplay.text(turnCounter);
   }
+  $turnDisplay.text(turnCounter);
+  $greenScore.text(greenPlayer);
+  $purpleScore.text(purplePlayer);
 }
 
 //this function determines whether to:
